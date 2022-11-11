df5c8cf032d1faef5f9159d7536263c2b43b619b
Maksim Ohvrill <589664@stud.hvl.no>
2022-11-11 13:33:25 +0100

    fixed controller & service
    added mapped put

--- Assignment 4 - Web Frameworks/item-rest-web-service/src/main/java/no/hvl/dat152/h2databaseexample/controller/ItemController.java
+++ Assignment 4 - Web Frameworks/item-rest-web-service/src/main/java/no/hvl/dat152/h2databaseexample/controller/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,24 @@ import no.hvl.dat152.h2databaseexample.service.ItemService;
 public class ItemController {
 	@Autowired
 	ItemService service;
+	
+	/*
+	 * GET /items: Return a list of all the items
+	 * POST /items: Add a new Item to the list
+	 * GET /items/{id}: Return the items given by "id" parameter
+	 * DELETE /items/{id}: Delete an item from the list
+	 * PUT /items/{id}: Update an item given by the "id" parameter and get updated information from the RequestBody. 
+	 */
 
 	@GetMapping("/items")
 	private List<Item> getAllItems() {
 		return service.getAllItems();
+	}
+	
+	@PostMapping("/items")
+	private int saveItem(@RequestBody Item item) {
+		service.saveOrUpdate(item);
+		return item.getId();
 	}
 
 	@GetMapping("/items/{id}")
@@ -33,9 +48,9 @@ public class ItemController {
 		service.delete(id);
 	}
 
-	@PostMapping("/items")
-	private int saveItem(@RequestBody Item item) {
-		service.saveOrUpdate(item);
-		return item.getId();
+	@PutMapping("/items/{id}")
+	private void updateItem(@PathVariable("id") int id, @RequestBody Item item) {
+		service.update(id, item);
 	}
+	
 }
--- Assignment 4 - Web Frameworks/item-rest-web-service/src/main/java/no/hvl/dat152/h2databaseexample/service/ItemService.java
+++ Assignment 4 - Web Frameworks/item-rest-web-service/src/main/java/no/hvl/dat152/h2databaseexample/service/ItemService.java
@@ -13,6 +13,14 @@ import no.hvl.dat152.h2databaseexample.repository.ItemRepository;
 public class ItemService {
 	@Autowired
 	ItemRepository itemRepository;
+	
+	/*
+	 * GET /items: Return a list of all the items
+	 * POST /items: Add a new Item to the list
+	 * GET /items/{id}: Return the item given by "id" parameter
+	 * DELETE /items/{id}: Delete an item from the list
+	 * PUT /items/{id}: Update an item given by the "id" parameter and get updated information from the RequestBody. 
+	 */
   
 	public List<Item> getAllItems() {
 		List<Item> items = new ArrayList<Item>();
@@ -20,16 +28,24 @@ public class ItemService {
 		return items;
 	}
 
-	public Item getItemById(int id) {
-		return itemRepository.findById(id).get();
-	}
-
 	public Item saveOrUpdate(Item item) {
 		itemRepository.save(item);
 		return item;
 	}
- 
+	
+	public Item getItemById(int id) {
+		return itemRepository.findById(id).get();
+	}
+
 	public void delete(int id) {
 		itemRepository.deleteById(id);
+	}
+	
+	public void update(int id, Item item) {
+		Item item1 = itemRepository.findById(id).get();
+		item1.setDescription(item.getDescription());
+		item1.setName(item.getName());
+		item1.setPrice(item.getPrice());
+		itemRepository.save(item1);
 	}
 }
