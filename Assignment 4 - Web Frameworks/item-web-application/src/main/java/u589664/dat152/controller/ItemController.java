a60d1eed5f122ea5b48d5344ae69c65513af5da2
Maksim Ohvrill <589664@stud.hvl.no>
2022-11-13 17:41:10 +0100

    delete mapping + language

--- Assignment 4 - Web Frameworks/item-web-application/src/main/java/u589664/dat152/controller/ItemController.java
+++ Assignment 4 - Web Frameworks/item-web-application/src/main/java/u589664/dat152/controller/ItemController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,6 +48,14 @@ public class ItemController {
 
         return "viewitem";
     }
+	
+	@GetMapping("/deleteitem")
+	public String deleteItem(@RequestParam String id) {
+		String url = "http://localhost:8299/items/" + id;
+        
+        restTemplate.delete(url, id);
+		return "redirect:/viewitems";
+	}
 	
 	@RequestMapping(value = "/createitem", method = RequestMethod.GET)
     protected String createItem(Model model) {
